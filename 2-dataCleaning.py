8155daf34a41a61507bcd4d25c44dfb883b9be4f
SwethaS <swethasaseendran4@gmail.com>
2021-06-10 14:59:10 +0530

    pre process and meta data analysis

--- 2-dataCleaning.py
+++ 2-dataCleaning.py
@@ -46,6 +46,25 @@ def remove_stopwords(text):
     words = [w for w in text if w not in stopwordsList]
     return words
 
+def text_cleaner(text):
+    rules = [
+        {r'>\s+': u'>'},  # remove spaces after a tag opens or closes
+        {r'\s+': u' '},  # replace consecutive spaces
+        {r'\s*<br\s*/?>\s*': u'\n'},  # newline after a <br>
+        {r'</(div)\s*>\s*': u'\n'},  # newline after </p> and </div> and <h1/>...
+        {r'</(p|h\d)\s*>\s*': u'\n\n'},  # newline after </p> and </div> and <h1/>...
+        {r'<head>.*<\s*(/head|body)[^>]*>': u''},  # remove <head> to </head>
+        {r'<a\s+href="([^"]+)"[^>]*>.*</a>': r'\1'},  # show links instead of texts
+        {r'[ \t]*<[^<]*?/?>': u''},  # remove remaining tags
+        {r'^\s+': u''}  # remove spaces at the beginning
+    ]
+    for rule in rules:
+        for (k, v) in rule.items():
+            regex = re.compile(k)
+            text = regex.sub(v, text)
+        text = text.rstrip()
+    return text
+
 lemmatizer = WordNetLemmatizer()
 def word_lemmatizer(text):
     lem_text = " ".join([lemmatizer.lemmatize(i) for i in text])
@@ -54,6 +73,7 @@ def word_lemmatizer(text):
 dataframe['processed_text'] = None
 tokenizer = RegexpTokenizer(r'\w+')
 
+dataframe.processed_text = dataframe.review.apply(lambda x: text_cleaner(x))
 dataframe.processed_text = dataframe.review.apply(lambda x: remove_punctuation(x), 1)
 dataframe.processed_text = dataframe.processed_text.apply(lambda x: tokenizer.tokenize(x.lower()))
 dataframe.processed_text = dataframe.processed_text.apply(lambda x: remove_stopwords(x))
@@ -61,5 +81,5 @@ dataframe.processed_text = dataframe.processed_text.apply(lambda x: word_lemmati
 dataframe.processed_text = dataframe.processed_text.apply(lambda elem: re.sub(r"([0-9]+)", "", elem))
 
 # Convert into a single CSV file.
-dataframe.to_csv('./clean_reviews.csv', index=False)
+dataframe.to_csv('./clean_reviews2.csv', index=False)
 print("CVS file is created")
